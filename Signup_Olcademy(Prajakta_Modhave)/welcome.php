e9aa361aeac90667ab33914100d4aaadc0e057f8
MODHAVE PRAJAKTA SUBHASH <68602923+PrajaktaModhave07@users.noreply.github.com>
2021-09-22 20:08:43 +0530

    Add files via upload

---
+++ Signup_Olcademy(Prajakta_Modhave)/welcome.php
@@ -0,0 +1,31 @@
+<?php 
+
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header("Location: index.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+</head>
+<body>
+    <?php echo "<h1>Welcome " . $_SESSION['username'] . "</h1>"; ?>
+    <a href="logout.php" style=" width: 100%;
+    height: 100%;
+    border: 2px solid #e7e7e7;
+    padding: 15px 20px;
+    font-size: 1rem;
+    border-radius: 30px;
+    background: transparent;
+    outline: none;
+    transition: .3s;
+	color: red ;" >Logout</a>
+</body>
+</html>
